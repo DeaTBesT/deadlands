549f9db19dddc7ff4742f299cbef62eb66c32b76
DeaTBesT <niliukhin@gmail.com>
2025-01-12 01:52:39 +0300

    feat: добавил класс манекена для отладки

---
+++ Assets/Scripts/DebugOptions/DummyStats.cs
@@ -0,0 +1,12 @@
+﻿using Core;
+using UnityEngine;
+
+namespace DebugOptions
+{
+    public class DummyStats : EntityStats
+    {
+        public override int TeamId => 999;
+        public override void TakeDamage(int teamId, float amount) => 
+            Debug.Log($"Take damage {amount} from team {teamId}");
+    }
+}
