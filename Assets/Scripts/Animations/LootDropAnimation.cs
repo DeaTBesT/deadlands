179954c1766d1e075536b37e6b7d023196088944
DeaTBesT <niliukhin@gmail.com>
2025-01-13 23:44:34 +0300

    refactor: небольшой рефакторинг

--- Assets/Scripts/Animations/LootDropAnimation.cs
+++ Assets/Scripts/Animations/LootDropAnimation.cs
@@ -7,6 +7,15 @@ namespace Animations
 {
     public static class LootDropAnimation
     {
+        /// <summary>
+        /// Анимация выпадения предмета.
+        /// </summary>
+        /// <param name="item">Предмет, который будет анимироваться.</param>
+        /// <param name="startPosition">Начальная позиция предмета.</param>
+        /// <param name="dropRadius">Радиус разброса предметов.</param>
+        /// <param name="dropHeight">Высота, на которую подбрасывается предмет.</param>
+        /// <param name="animationDuration">Длительность анимации.</param>
+        /// <param name="onComplete">Событие, вызываемое по завершении анимации.</param>
         public static void AnimateItemDrop(this GameObject item, Vector3 startPosition, float dropRadius, float dropHeight,
             float animationDuration, Action onComplete = null)
         {
@@ -20,7 +29,6 @@ namespace Animations
 
             var endPosition = startPosition + randomOffset;
 
-            // Вычисляем контрольную точку для кривой (с подъёмом вверх)
             var controlPoint = (startPosition + endPosition) / 2 + Vector3.up * dropHeight;
 
             var sequence = DOTween.Sequence();
