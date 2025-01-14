24e061bd89940211d1f31c8bffc00bde9e4aedf0
DeaTBesT <niliukhin@gmail.com>
2025-01-14 12:38:49 +0300

    refactor: разделил PlacePanelUI на 2 класса

---
+++ Assets/Scripts/UI/PlacePanels/Core/AdvancedPlacePanelUI.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using GameResources.UI;
+using Place.Core;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.PlacePanels.Core
+{
+    public class AdvancedPlacePanelUI : SimplePlacePanelUI
+    {
+        [SerializeField] protected Button _button;
+        [SerializeField] protected Transform _requiredResourcesParent;
+        [SerializeField] protected ResourceDataUI _requiredResourcesPrefab;
+        
+        protected List<ResourceDataUI> _resourcesDataUI = new();
+
+        protected PlaceController _placeController;
+        
+        public override void Initialize(params object[] objects) => 
+            _placeController = objects[0] as PlaceController;
+        
+        protected void ClearRequiredResourcesPanel()
+        {
+            if (_requiredResourcesParent == null)
+            {
+#if UNITY_EDITOR
+                Debug.LogError("Resource panels is null");
+#endif
+                return;
+            }
+
+            foreach (Transform child in _requiredResourcesParent)
+            {
+                Destroy(child.gameObject);
+            }
+
+            _resourcesDataUI.Clear();
+        }
+    }
+}
---
+++ Assets/Scripts/UI/PlacePanels/Core/SimplePlacePanelUI.cs
@@ -0,0 +1,20 @@
+﻿using Interfaces;
+using UnityEngine;
+
+namespace UI.PlacePanels.Core
+{
+    public abstract class SimplePlacePanelUI : MonoBehaviour, IInitialize, IPlacePanel
+    {
+        [SerializeField] protected GameObject _renderer;
+        
+        public virtual void Show() =>
+            _renderer.SetActive(true);
+
+        public virtual void Hide() => 
+            _renderer.SetActive(false);
+
+        public bool IsEnable { get; set; }
+        
+        public abstract void Initialize(params object[] objects);
+    }
+}
