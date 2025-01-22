f40b1bce642f26366f638da76e8318bcdda97328
DeaTBesT <niliukhin@gmail.com>
2025-01-22 17:32:16 +0300

    feat: убрал возмодность добавления нескольких одиннаковых компонентов

--- Assets/Scripts/Structure/UIPanels/StructuresPanels/GeneralPanelUI.cs
+++ Assets/Scripts/Structure/UIPanels/StructuresPanels/GeneralPanelUI.cs
@@ -1,7 +1,10 @@
 ﻿using DL.StructureRuntime.UIPanels.Core;
+using UnityEngine;
 
 namespace DL.StructureRuntime.UIPanels.StructuresPanels
 {
+    //Главная панель, может быть только одна
+    [DisallowMultipleComponent]
     public class GeneralPanelUI : SimpleStructurePanelUI
     {
         public override void Initialize(params object[] objects)
