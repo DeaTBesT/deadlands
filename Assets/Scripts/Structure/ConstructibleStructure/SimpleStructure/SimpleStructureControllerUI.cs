1620a6ea2602b1e18add175edb3841d2e71220f3
DeaTBesT <niliukhin@gmail.com>
2025-01-22 05:50:27 +0300

    feat: добавли простую струтуру

---
+++ Assets/Scripts/Structure/ConstructibleStructure/SimpleStructure/SimpleStructureControllerUI.cs
@@ -0,0 +1,13 @@
+﻿using DL.ConstructibleStructureRuntime.Core;
+
+namespace Structure.ConstructibleStructure.SimpleStructure
+{
+    public class SimpleStructureControllerUI : ConstructibleStructureControllerUI
+    {
+        public override void Interact() => 
+            OpenPanel();
+
+        public override void FinishInteract() => 
+            ClosePanels();
+    }
+}
