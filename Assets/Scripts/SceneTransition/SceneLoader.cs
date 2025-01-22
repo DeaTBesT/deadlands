fd284369dc97c46f12243aeb44237abc1881b11b
DeaTBesT <niliukhin@gmail.com>
2025-01-22 05:57:23 +0300

    feat: добавил загрузчик сцен

---
+++ Assets/Scripts/SceneTransition/SceneLoader.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections;
+using DL.InterfacesRuntime;
+using DL.UtilsRuntime;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace DL.SceneTransitionRuntime
+{
+    public class SceneLoader : Singleton<SceneLoader>, IInitialize
+    {
+        [SerializeField] private GameObject _canvasLoad;
+        [SerializeField] private Slider _loadingBar;
+        
+        [SerializeField] private Canvas _canvasPrefab;
+        
+        private Coroutine _sceneLoadingRoutine;
+
+        public static Action<Scene> OnStartLoadScene { get; set; }
+        public static Action OnLoadingScene { get; set; }
+        public static Action<Scene> OnFinishLoadScene { get; set; }
+
+        public bool IsEnable { get; set; } = true;
+        
+        public void Initialize(params object[] objects)
+        {
+            if (_canvasLoad == null)
+            {
+                _canvasLoad = Instantiate(_canvasPrefab.gameObject);
+                _loadingBar = _canvasLoad.GetComponentInChildren<Slider>();
+            }
+
+            _canvasLoad.SetActive(false);
+        }
+
+        public void LoadScene(Scene newScene)
+        {
+            if (_sceneLoadingRoutine != null)
+            {
+#if UNITY_EDITOR
+                Debug.LogWarning("Player transitioning already");
+#endif
+                return;
+            }
+
+#if UNITY_EDITOR
+            Debug.Log($"Start transition new scene: {newScene}");
+#endif
+            _sceneLoadingRoutine = StartCoroutine(LoadSceneRoutine(newScene));
+        }
+
+        private IEnumerator LoadSceneRoutine(Scene newScene)
+        {
+            _canvasLoad.SetActive(true);
+            OnStartLoadScene?.Invoke(newScene);
+            var operation = SceneManager.LoadSceneAsync(newScene.ToString());
+
+            yield return new WaitUntil(() =>
+            {
+                _loadingBar.value = operation.progress;
+                OnLoadingScene?.Invoke();
+                return operation.isDone;
+            });
+            
+            OnFinishLoadScene?.Invoke(newScene);
+            _canvasLoad.SetActive(false);
+        }
+    }
+}
