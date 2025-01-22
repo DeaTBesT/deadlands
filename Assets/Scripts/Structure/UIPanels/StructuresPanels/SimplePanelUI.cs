877dcb08ab0533d1dd074bb4783ea9d49e0cdd45
DeaTBesT <niliukhin@gmail.com>
2025-01-22 17:32:29 +0300

    feat: добавли простую панель

---
+++ Assets/Scripts/Structure/UIPanels/StructuresPanels/SimplePanelUI.cs
@@ -0,0 +1,13 @@
+﻿using DL.StructureRuntime.UIPanels.Core;
+
+namespace DL.StructureRuntime.UIPanels.StructuresPanels
+{
+    //Второстепенная панель, может быть несколько
+    public class SimplePanelUI : SimpleStructurePanelUI
+    {
+        public override void Initialize(params object[] objects)
+        {
+            
+        }
+    }
+}
