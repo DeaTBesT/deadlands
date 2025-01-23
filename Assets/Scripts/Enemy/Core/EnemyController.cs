f4eafd33e85c9b280dacc5ffe9f7d929e5d0967c
DeaTBesT <niliukhin@gmail.com>
2025-01-23 17:01:46 +0300

    feat: сделал базу для врагов

---
+++ Assets/Scripts/Enemy/Core/EnemyController.cs
@@ -0,0 +1,31 @@
+﻿using DL.CoreRuntime;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace DL.StructureRuntime.UIPanels.Core
+{
+    public class EnemyController : EntityController
+    {
+        private Transform _player;
+        private NavMeshAgent _navMeshAgent;
+        
+        public override void Initialize(params object[] objects)
+        {
+            _player = objects[0] as Transform;
+            _navMeshAgent = objects[1] as NavMeshAgent;
+        }
+
+        public override void ActivateEntity()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void DiactivateEntity()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private void Update() => 
+            _navMeshAgent.SetDestination(_player.position);
+    }
+}
