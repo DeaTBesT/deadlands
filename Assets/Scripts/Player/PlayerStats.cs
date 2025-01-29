8cf0fec1706f3cbaf46844d134843decf5c37ec6
DeaTBesT <niliukhin@gmail.com>
2025-01-29 17:51:30 +0300

    refactor: указал команду

--- Assets/Scripts/Player/PlayerStats.cs
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -1,10 +1,11 @@
-﻿using DL.CoreRuntime;
+﻿using Data;
+using DL.CoreRuntime;
 
 namespace DL.PlayersRuntime
 {
     public class PlayerStats : EntityStats
     {
-        public override int TeamId => 1;
+        public override int TeamId => Teams.PlayerTeamId;
 
         public override void TakeDamage(int teamId, float amount)
         {
