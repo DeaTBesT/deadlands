494987b650028e96411993214a220ebdc6f36aed
DeaTBesT <niliukhin@gmail.com>
2025-02-02 18:14:33 +0300

    refactor: поменял систему инвентаря, вынес в отдельный модуль

---
+++ Assets/Scripts/Core/IInventoryController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using DL.Data.Resource;
+using DL.InterfacesRuntime;
+
+namespace DL.CoreRuntime
+{
+    public interface IInventoryController : IInitialize, IDeinitialize
+    {
+        public Action<ResourceDataModel> OnAddResource { get; set; }
+        public Action<List<ResourceDataModel>> OnChangedResourcesData { get; set; }
+        public Action<ResourceDataModel> OnRemoveResource { get; set; }
+        public Action<ResourceDataModel> OnDropResource { get; set; }
+        public Action OnDropAllResources { get; set; }
+
+        void SetInfinityInventory(bool isInfinity);
+        
+        bool TryAddResource(ResourceDataModel resourceData);
+        void RemoveResource(ResourceDataModel resourceData);
+        
+        void DropResource(ResourceDataModel resourceData);
+        void DropAllResources();
+
+        void AddResources(List<ResourceDataModel> resourcesData);
+        List<ResourceDataModel> TakeResources();
+    }
+}
---
+++ Assets/Scripts/InventorySystem/Core/InventoryController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using DL.CoreRuntime;
+using DL.Data.Resource;
+using UnityEngine;
+
+namespace DL.InventorySystem.Core
+{
+    public abstract class InventoryController : MonoBehaviour, IInventoryController
+    {
+        protected bool _isInfinityInventory;
+        
+        public Action<ResourceDataModel> OnAddResource { get; set; }
+        public Action<List<ResourceDataModel>> OnChangedResourcesData { get; set; }
+        public Action<ResourceDataModel> OnRemoveResource { get; set; }
+        public Action<ResourceDataModel> OnDropResource { get; set; }
+        public Action OnDropAllResources { get; set; }
+
+        public bool IsEnable { get; set; }
+
+        public virtual void Initialize(params object[] objects)
+        {
+        }
+
+        public virtual void Deinitialize(params object[] objects)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SetInfinityInventory(bool isInfinity) => 
+            _isInfinityInventory = isInfinity;
+
+        public abstract bool TryAddResource(ResourceDataModel resourceData);
+
+        public abstract void RemoveResource(ResourceDataModel resourceData);
+
+        public abstract void DropResource(ResourceDataModel resourceData);
+
+        public abstract void DropAllResources();
+
+        public virtual void AddResources(List<ResourceDataModel> resourcesData)
+        {
+        }
+
+        public virtual List<ResourceDataModel> TakeResources()
+        {
+            return null;
+        }
+    }
+}
