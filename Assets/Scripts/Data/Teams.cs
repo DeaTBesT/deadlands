b66c426dc74447706065749360a31440c1ad4c06
DeaTBesT <niliukhin@gmail.com>
2025-02-03 20:36:01 +0300

    feat: добавил комманду для манекена

--- Assets/Scripts/Data/Teams.cs
+++ Assets/Scripts/Data/Teams.cs
@@ -3,5 +3,6 @@
     public static class Teams
     {
         public const int PlayerTeamId = 0;
+        public const int DummyTeamId = 999;
     }
 }
--- Assets/Scripts/DebugOptions/DummyStats.cs
+++ Assets/Scripts/DebugOptions/DummyStats.cs
@@ -1,13 +1,23 @@
-﻿using DL.CoreRuntime;
+﻿using Data;
+using DL.CoreRuntime;
 using UnityEngine;
 
 namespace DL.DebugOptionsRuntime
 {
     public class DummyStats : EntityStats
     {
-        public override int TeamId => 999;
+        public override int TeamId => Teams.DummyTeamId;
+
         public override bool TryApplyDamage(int teamId, float amount)
+        {
+            if (teamId == Teams.DummyTeamId)
+            {
+                return false;
+            }
+            
             Debug.Log($"Take damage {amount} from team {teamId}");
+
             return true;
+        }
     }
 }
