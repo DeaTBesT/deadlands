a799ff9d8396332e40531d75ca4853a003810863
DeaTBesT <niliukhin@gmail.com>
2025-02-09 15:36:54 +0300

    feat: добавил менеджер для дебага

--- Assets/Scripts/Managers/GameStarter.cs
+++ Assets/Scripts/Managers/GameStarter.cs
@@ -5,6 +5,7 @@ namespace DL.ManagersRuntime
     public static class GameStarter
     {
         private const string ManagersPath = "Managers";
+        private const string DebugManagersPath = "DebugManager";
 
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
         private static void InitializeManagers()
@@ -15,9 +16,25 @@ namespace DL.ManagersRuntime
             {
                 return;
             }
-            
+
             var managersObj = Resources.Load(ManagersPath);
             Object.Instantiate(managersObj);
         }
+
+#if UNITY_EDITOR
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        private static void InitializeDebugManager()
+        {
+            var manager = Object.FindObjectOfType<DebugManager>();
+
+            if (manager != null)
+            {
+                return;
+            }
+
+            var managerObj = Resources.Load(DebugManagersPath);
+            Object.Instantiate(managerObj);
+        }
+#endif
     }
 }
