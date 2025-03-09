7a2203e90e6407134a4f4627c77dec46b55556ab
DeaTBesT <niliukhin@gmail.com>
2025-03-09 23:40:22 +0300

    refactor: убрал лишие поля

--- Assets/Scripts/Data/Wardrobe/Configs/WardrobeItemConfig.cs
+++ Assets/Scripts/Data/Wardrobe/Configs/WardrobeItemConfig.cs
@@ -6,16 +6,10 @@ namespace DL.Data.Wardrobe
 {
     public class WardrobeItemConfig : ItemConfig
     {
-        [SerializeField] private string _title;
-        [SerializeField] private string _description;
-        [SerializeField] private Sprite _icon;
         [SerializeField] private int _maxParts; //Макс. кол-во частей, нужное для сбора предмета
         [SerializeField] private RareType _rareType;
         [SerializeField] private WardrobeItemType _wardrobeItemType;
 
-        public string Title => _title;
-        public string Description => _description;
-        public Sprite Icon => _icon;
         public int MaxParts => _maxParts;
         public RareType TypeRare => _rareType;
         public WardrobeItemType ItemType => _wardrobeItemType;
