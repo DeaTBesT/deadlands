d33926906a5dd2ca48dac1bdd4cde1f66822fd5d
DeaTBesT <niliukhin@gmail.com>
2025-03-16 01:17:22 +0300

    feat: добавил конвертер типа редкости в шанс выпадения

---
+++ Assets/Scripts/Utils/RareChanceConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using DL.EnumsRuntime;
+
+namespace DL.UtilsRuntime
+{
+    public static class RareChanceConverter
+    {
+        private const int GreyChance = 50;
+        private const int BlueChance = 30;
+        private const int PurpleChance = 20;
+        private const int GoldChance = 10;
+
+        public static int GetChance(RareType rareType)
+        {
+            return rareType switch
+            {
+                RareType.Grey => GreyChance,
+                RareType.Blue => BlueChance,
+                RareType.Purple => PurpleChance,
+                RareType.Gold => GoldChance,
+                _ => throw new ArgumentOutOfRangeException(nameof(rareType), rareType, null)
+            };
+        }
+    }
+}
